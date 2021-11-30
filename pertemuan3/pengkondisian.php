3cd46346c02df14b81a45771e2b9ea390d0b1bde
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-11-30 18:20:45 +0700

    Create pengkondisian.php

---
+++ pertemuan3/pengkondisian.php
@@ -0,0 +1,14 @@
+<?php
+  // pertemuan 3 - pengkondisian / percabangan
+  
+  // if, else if, else, ternary, switch
+  
+  $x = 10;
+  if ($x<5) {
+    echo "benar";
+  } else if ($x==10) {
+    echo "bingo!";
+  } else {
+    echo "salah";
+  }
+?>
