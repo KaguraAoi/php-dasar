a565d6a9e4d083e824c0fe354e4844f535c91ed0
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-02 13:12:48 +0700

    Create metodeget.php

---
+++ pertemuan7/get/metodeget.php
@@ -0,0 +1,7 @@
+<?php
+  // pertemuan 7 - metode get
+
+  // metode get - metode pengiriman data berupa string melalui url
+  // data ditambahkan setelah nama file php lalu tanda ? misal latihan.php?nama=aditya atha
+  // jika ingin menambahkan data menggunakan tanda & misal latihan.php?nama=aditya atha&nim=192102118
+?>
