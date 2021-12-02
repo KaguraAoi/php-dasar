9619037d4f5c6801b39e8a0c4c7881f0f418906d
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-02 11:35:05 +0700

    Create looparray.php

---
+++ pertemuan5/looparray.php
@@ -0,0 +1,40 @@
+<?php
+  // pertemuan 5 - looping pada array
+
+  $angka = [3, 2, 15, 20, 11, 77, 89];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>latihan array</title>
+  <style>
+    .kotak {
+      width: 50px;
+      height: 50px;
+      background-color: salmon;
+      text-align: center;
+      line-height: 50px;
+      margin: 3px;
+      float: left;
+    }
+    
+    .clear {
+      clear: both;
+    }
+  </style>
+</head>
+<body>
+  <!-- menggunakan for -->
+  <?php for ($i=0; $i<count($angka); $i++) : ?>
+    <div class="kotak"><?php echo "$angka[$i]"; ?></div>
+  <?php endfor; ?>
+  
+  <div class="clear"></div>
+  
+  <!-- menggunakan foreach -->
+  <?php foreach ($angka as $a) : ?>
+    <div class="kotak"><?php echo "$a"; ?></div>
+  <?php endforeach; ?>
+</body>
+</html>
